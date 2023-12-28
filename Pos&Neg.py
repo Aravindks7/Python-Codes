514c766c27560dbd63db9d1bbeec92dc1a82ee34
Aravindks7 <92859388+Aravindks7@users.noreply.github.com>
2023-12-29 00:21:39 +0530

    Delete Pos&Neg.py

--- Pos&Neg.py
+++
@@ -1,7 +0,0 @@
-print("To check if given number is positive or negative")
-n=int(input("Enter a number: "))
-
-if(n>=0):
-    print(f"{n} is a positive number")
-else:
-    print(f"{n} is a negative number")
